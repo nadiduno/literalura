f11eb0d8237c879701ff5d923bdaa2698eacab07
nadiduno <nadiduno@gmail.com>
2025-02-09 20:31:00 -0300

    Connecting API

--- src/main/java/com/livros/literalura/LiteraluraApplication.java
+++ src/main/java/com/livros/literalura/LiteraluraApplication.java
@@ -1,13 +1,24 @@
 package com.livros.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.livros.literalura.service.ConnectAPI;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var connectApi = new ConnectAPI();
+		var json = connectApi.obterDados("https://gutendex.com/books/?search=casa+velha");
+		System.out.println(json);
+		
 	}
 
 }
